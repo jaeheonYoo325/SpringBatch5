95cc8a6d078d8ef06999cd344569e0e423bad336
jaeheonYoo325 <yoojh0325@gmail.com>
2025-04-01 21:51:17 +0900

    배치처리1: 테이블 to  테이블.

---
+++ src/main/java/com/springbatch/edu/SpringBatch5/repository/AfterRepository.java
@@ -0,0 +1,7 @@
+package com.springbatch.edu.SpringBatch5.repository;
+
+import com.springbatch.edu.SpringBatch5.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+}
---
+++ src/main/java/com/springbatch/edu/SpringBatch5/repository/BeforeRepository.java
@@ -0,0 +1,7 @@
+package com.springbatch.edu.SpringBatch5.repository;
+
+import com.springbatch.edu.SpringBatch5.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+}
