01fb3e13b5399dea5942f2bd292847c311f4caea
jaeheonYoo325 <yoojh0325@gmail.com>
2025-04-01 18:43:43 +0900

    프로젝트 생성 및 DB연결 설정.

---
+++ src/main/java/com/springbatch/edu/SpringBatch5/config/MetaDBConfig.java
@@ -0,0 +1,31 @@
+package com.springbatch.edu.SpringBatch5.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MetaDBConfig {
+    @Primary
+    @Bean
+    public PlatformTransactionManager metaTransactionManager() {
+        return new DataSourceTransactionManager(metaDBSource());
+    }
+
+
+    @Primary
+    @Bean
+    public DataSource metaDBSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:mysql://192.168.111.100:3308/springbatch_meta_db?useSSL=false&useUnicode=true&serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true")
+                .username("root")
+                .password("password")
+                .build();
+    }
+}
