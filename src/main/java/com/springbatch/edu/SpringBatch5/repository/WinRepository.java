3fb777d8aab26a4a5fdce46e6105dd8a3b836df8
jaeheonYoo325 <yoojh0325@gmail.com>
2025-04-05 12:05:03 +0900

    배치 처리2 : 테이블 조건(게임 보상).

---
+++ src/main/java/com/springbatch/edu/SpringBatch5/repository/WinRepository.java
@@ -0,0 +1,10 @@
+package com.springbatch.edu.SpringBatch5.repository;
+
+import com.springbatch.edu.SpringBatch5.entity.WinEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WinRepository extends JpaRepository<WinEntity, Long> {
+    Page<WinEntity> findByWinGreaterThanEqual(Long win, Pageable pageable);
+}
